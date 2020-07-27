7b3f215181e54b66ffb0dd0cb32a0255a667b145
sameera <sameera.bachoti@gmail.com>
2020-07-26 20:27:47 -0400

    Add validations to journal entry form

--- app/src/NewEntry.js
+++ app/src/NewEntry.js
@@ -21,6 +21,11 @@ class NewEntry extends React.Component {
 	    super(props);
 	    this.state = {
 	    	entry: this.emptyEntry,
+	    	errors: {
+	            title: '',
+	            category: '',
+	            content: '',
+	          }
 	    };
 	    this.handleChange = this.handleChange.bind(this);
 	    this.handleSubmit = this.handleSubmit.bind(this);
@@ -85,15 +90,28 @@ class NewEntry extends React.Component {
 	    const value = target.value;
 	    const name = target.name;
 	    let entry = {...this.state.entry};
+	    let errors = this.state.errors;
+	    
 	    entry[name] = value;
 	    if(name=='category'){
+	    	errors.category = value === '' ? 'Category is required' : '';
 	    	entry[name] = {name: value}
 	    }
+	    else if(name == 'title'){
+	    	errors.title = value === '' ? 'Title is required' : '';
+	    }
+	    else if(name == 'content'){
+	    	errors.content = value === '' ? 'Entry is required' : '';
+	    }
+	    
+	    this.setState({errors, [name]: value});
 	    this.setState({entry});
 	}
 	
 	render() {
 		const {entry} = this.state;
+		const {errors} = this.state;
+		let isDisabled = errors.title || errors.category || errors.content;
 		
 		const divStyle = {
 				'margin-left': '300px', 
@@ -104,6 +122,11 @@ class NewEntry extends React.Component {
 				'border-radius': '10px'
 		};
 		
+		const error = {
+			'font-size': '15px',
+			'color': 'red'
+		}
+		
 		return(
 				<Container>
 		         <Form onSubmit={this.handleSubmit} style={divStyle}>
@@ -111,19 +134,22 @@ class NewEntry extends React.Component {
 		            <Label for="title">Title</Label>
 		            <Input type="text"style={{width: "500px"}} name="title" id="title" value={entry.title || ''}
 		                   onChange={this.handleChange} autoComplete="title"/>
+		            <span style={error}>{errors.title}</span>
 		          </FormGroup>
 		          <FormGroup>
 		            <Label for="category">Category</Label>
 		            <Input type="text" style={{width: "500px"}} name="category" id="category" value={entry.category.name || ''}
 		                   onChange={this.handleChange} autoComplete="title"/>
+		            <span style={error}>{errors.category}</span>
 		          </FormGroup>
 		          <FormGroup>
 		            <Label for="content">Entry</Label>
 		            <Input type="textarea" style={{width: "500px", height: "100px"}} name="content" id="content" value={entry.content || ''}
 		                   onChange={this.handleChange} autoComplete="content"/>
+		            <span style={error}>{errors.content}</span>
 		          </FormGroup>
 		          <FormGroup>
-		            <Button color="primary" type="submit">Save</Button>{' '}
+		            <Button color="primary" type="submit" disabled={isDisabled}>Save</Button>{' '}
 		            <Button color="secondary" to="/">Cancel</Button>
 		          </FormGroup>
 		        </Form>
