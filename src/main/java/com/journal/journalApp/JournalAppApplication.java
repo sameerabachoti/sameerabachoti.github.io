0c957db08cecc8e20ad0b79ce2bf55f31b090412
sameera <sameera.bachoti@gmail.com>
2020-07-27 21:01:20 -0400

    Fix whitelabel error

--- src/main/java/com/journal/journalApp/JournalAppApplication.java
+++ src/main/java/com/journal/journalApp/JournalAppApplication.java
@@ -2,9 +2,10 @@ package com.journal.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JournalAppApplication {
+public class JournalAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JournalAppApplication.class, args);
